b81c8257dfa80fe475812efe444fc704fdc518bd
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2023-11-16 21:29:03 +0100

    Added countdown

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,7 +1,39 @@
+import { useEffect, useState } from "react";
 import "./Countdown.css";
 
-const Countdown = () => {
-  return <div>Countdown</div>;
+const Countdown = ({ countdownTime }) => {
+  const [time, setTime] = useState(countdownTime);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime((value) => value - 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  let seconds = time;
+  const days = Math.floor(seconds / (24 * 60 * 60));
+  seconds %= 24 * 60 * 60;
+  const hours = Math.floor(seconds / (60 * 60));
+  seconds %= 60 * 60;
+  const minutes = Math.floor(seconds / 60);
+  seconds %= 60;
+
+  return (
+    <section className="countdown">
+      <div className="countdown__number countdown__number--days">
+        {days < 10 ? `0${days}` : days}
+      </div>
+      <div className="countdown__number countdown__number--hours">{hours}</div>
+      <div className="countdown__number countdown__number--minutes">
+        {minutes}
+      </div>
+      <div className="countdown__number countdown__number--seconds">
+        {seconds}
+      </div>
+    </section>
+  );
 };
 
 export default Countdown;
